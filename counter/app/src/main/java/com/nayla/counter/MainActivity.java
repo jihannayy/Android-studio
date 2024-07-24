ae277ecfa6916a689d47e6c25ff521fcb10c9e28
jihannayy <140767336+jihannayy@users.noreply.github.com>
2024-07-24 14:28:11 +0700

    video 6
    android studio

--- counter/app/src/main/java/com/nayla/counter/MainActivity.java
+++ counter/app/src/main/java/com/nayla/counter/MainActivity.java
@@ -1,6 +1,8 @@
 package com.nayla.counter;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,37 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    int count= 0;
+    TextView tvHasil;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        load();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+
         });
+    }
+
+    public void load (){
+        tvHasil = findViewById(R.id.tvHasil);
+    }
+
+    public void btnUp(View view) {
+        count++;
+        tvHasil.setText(count+"");
+    }
+
+    public void btnDown(View view) {
+        count--;
+        tvHasil.setText(count+"");
     }
 }
