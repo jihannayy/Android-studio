8d85b3aab6083c70f61600074ac266c8ca36abf5
jihannayy <140767336+jihannayy@users.noreply.github.com>
2024-08-01 08:59:24 +0700

    Video 21
    Android Studio

---
+++ RecyclerCardView/app/src/main/java/com/nayla/recyclercardview/SiswaAdapter.java
@@ -0,0 +1,57 @@
+package com.nayla.recyclercardview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class SiswaAdapter extends RecyclerView.Adapter<SiswaAdapter.Viewolder> {
+
+    private Context context;
+    private List<Siswa>siswaList;
+
+    public SiswaAdapter(Context context, List<Siswa> siswaList) {
+        this.context = context;
+        this.siswaList = siswaList;
+    }
+
+    @NonNull
+    @Override
+    public Viewolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_siswa, viewGroup, false);
+        return new Viewolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Viewolder viewHolder, int i) {
+        Siswa siswa = siswaList.get(i);
+        viewHolder.tvNama.setText(siswa.getNama());
+        viewHolder.tvAlamat.setText(siswa.getNama());
+    }
+
+
+
+    @Override
+    public int getItemCount() {
+        return siswaList.size();
+    }
+
+    public class Viewolder extends RecyclerView.ViewHolder{
+
+        TextView tvNama, tvAlamat;
+
+
+        public Viewolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvNama = itemView.findViewById(R.id.tvNama);
+            tvAlamat = itemView.findViewById(R.id.tvAlamat);
+        }
+    }
+}
