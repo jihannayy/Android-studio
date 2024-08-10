57a02454ffbf1705d798e17231072bacebc1711a
jihannayy <140767336+jihannayy@users.noreply.github.com>
2024-08-10 17:17:46 +0700

    video 33
    Android Studio

--- SQLiteDatabase/app/src/main/java/com/nayla/sqlitedatabase/BarangAdapter.java
+++ SQLiteDatabase/app/src/main/java/com/nayla/sqlitedatabase/BarangAdapter.java
@@ -1,6 +1,7 @@
 package com.nayla.sqlitedatabase;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -22,12 +23,16 @@ public class BarangAdapter extends RecyclerView.Adapter<BarangAdapter.ViewHolder
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_barang, viewGroup, false);
+        return new ViewHolder(v);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        viewHolder.tvBarang.setText(barangList.get(i).getBarang());
+        viewHolder.tvStok.setText(barangList.get(i).getStok());
+        viewHolder.tvHarga.setText(barangList.get(i).getHarga());
 
     }
 
