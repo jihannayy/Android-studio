c05dde50faaa7d0ce535adbc09265a1b5539794c
jihannayy <140767336+jihannayy@users.noreply.github.com>
2024-08-10 18:18:56 +0700

    video 34
    Android Studio

--- SQLiteDatabase/app/src/main/java/com/nayla/sqlitedatabase/BarangAdapter.java
+++ SQLiteDatabase/app/src/main/java/com/nayla/sqlitedatabase/BarangAdapter.java
@@ -2,9 +2,12 @@ package com.nayla.sqlitedatabase;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -33,6 +36,34 @@ public class BarangAdapter extends RecyclerView.Adapter<BarangAdapter.ViewHolder
         viewHolder.tvBarang.setText(barangList.get(i).getBarang());
         viewHolder.tvStok.setText(barangList.get(i).getStok());
         viewHolder.tvHarga.setText(barangList.get(i).getHarga());
+
+        viewHolder.tvMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                PopupMenu popupMenu = new PopupMenu(context, viewHolder.tvMenu);
+                popupMenu.inflate(R.menu.menu_item);
+
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+
+                       int menuItemId = menuItem.getItemId();
+                        if (menuItemId == R.id.ubah){
+                            Toast.makeText(context, "UBAH", Toast.LENGTH_SHORT).show();
+
+                        }else {
+                            Toast.makeText(context, "HAPUS", Toast.LENGTH_SHORT).show();
+                        }
+
+
+                        return false;
+                    }
+                });
+
+                popupMenu.show();
+            }
+        });
 
     }
 
